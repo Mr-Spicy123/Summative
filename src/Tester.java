04ccb222be9979ef4e0f7a57d915db04e34acbb1
Mr-Spicy123 <jspic1@ocdsb.ca>
2024-06-05 06:32:08 -0400

    frame images added

--- src/Tester.java
+++ src/Tester.java
@@ -5,5 +5,6 @@ public class Tester {
   public static void main(String[] args) {
     //Writer.writeUserInfo("hello world!");
     System.out.println(Reader.readLastUserInfo());
+    Writer.writeUserInfo("jackson is the best");
   }
 }
