ebb1da10ca4e503e4902f0dc92f7715957768735
Mr-Spicy123 <jspic1@ocdsb.ca>
2024-06-07 08:07:50 -0400

    chrome opener!

--- src/verificationFrame.java
+++ src/verificationFrame.java
@@ -22,6 +22,13 @@ public class verificationFrame {
           JButton button = (JButton) source;
           String currentItem = "";
           switch (button.getText()) {
+            case "back":
+              new LoginFrame();
+              verificationFrame.setVisible(false);
+              break;
+            case "enter":
+              //String
+              break;
           }
         }
       }
@@ -38,6 +45,15 @@ public class verificationFrame {
 
     JButton backButton = new JButton("back");
     SwingSetup.setupButton(backButton, objectPanel, 20, 10, 200, 100, false, false);
+    backButton.addActionListener(listener);
+
+    JButton enterButton = new JButton("enter");
+    SwingSetup.setupButton(enterButton, objectPanel, 1080, 415, 325, 125, false, false);
+    enterButton.addActionListener(listener);
+
+    emailUsername = new JTextField();
+    SwingSetup.setupTextField(emailUsername, objectPanel, 567, 440, 385, 75, true);
+    emailUsername.setFont(font);
 
     SwingSetup.setupFrame(verificationFrame, 0, 0, 1536, 864, false, null);
 
